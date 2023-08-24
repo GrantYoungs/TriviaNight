30b570b79d1277dce37a975e4f6dab69c9a3b24b
GrantYoungs <grantpyoungs97@gmail.com>
2023-08-24 16:45:23 -0400

    Setup Hilt for Dependency Injection

---
+++ app/src/main/java/com/example/trivianight/TriviaNight.kt
@@ -0,0 +1,9 @@
+package com.example.trivianight
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TriviaNight : Application() {
+
+}
--- app/src/main/java/com/example/trivianight/TriviaNightHome.kt
+++ app/src/main/java/com/example/trivianight/TriviaNightHome.kt
@@ -12,8 +12,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.trivianight.ui.theme.TriviaNightTheme
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : ComponentActivity() {
+@AndroidEntryPoint
+class TriviaNightHome : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
